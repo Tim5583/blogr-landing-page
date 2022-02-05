15e75c3c57a345b626eb1b4f01af7851dace5c0f
Praveen Thamindu <praveenthamindu@gmail.com>
2022-02-05 16:35:05 +0530

    finish desktop navigation bar

--- js/script.js
+++ js/script.js
@@ -2,9 +2,16 @@ const dropItems = document.getElementsByClassName("drop-title");
 const hamburgerIcon = document.getElementById("hamburger-icon");
 const navMenu = document.querySelector(".nav-menu");
 const closeIcon = document.querySelector(".close-icon");
+let btn ;
 
 for (let i = 0; i < dropItems.length; i++) {
     dropItems[i].addEventListener("click", (e) => {
+        if (btn != e.target.textContent) {
+            Array.from(dropItems).forEach((item) => {
+                (item.nextElementSibling).classList.remove("show-drop-items");
+            });
+            btn = e.target.textContent;
+        }
         e.target.nextElementSibling.classList.toggle("show-drop-items");
     })
 }
